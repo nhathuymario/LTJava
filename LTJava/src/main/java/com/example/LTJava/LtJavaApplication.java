d919372472d0a4162f5fb51a213eb50762f380f0
nhathuymario <huyne1501@gmail.com>
2025-12-06 11:32:47 +0700

    .

---
+++ LTJava/src/main/java/com/example/LTJava/LtJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LTJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtJavaApplication.class, args);
+	}
+
+}
