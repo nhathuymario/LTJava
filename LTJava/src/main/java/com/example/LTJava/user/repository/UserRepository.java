5b707961f9bbde88e638d1ea9598f246fe84ad70
nhathuymario <huyne1501@gmail.com>
2025-12-07 14:41:06 +0700

    add user repository and role

---
+++ LTJava/src/main/java/com/example/LTJava/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.LTJava.user.repository;
+
+import com.example.LTJava.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
