f4a5a354835e82a725f50a94f037230018f69792
nhathuymario <huyne1501@gmail.com>
2025-12-15 14:01:17 +0700

    add dto

---
+++ LTJava/src/main/java/com/example/LTJava/cousrse/dto/CreateCourseRequest.java
@@ -0,0 +1,12 @@
+package com.example.LTJava.course.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CreateCourseRequest {
+    private String code;
+    private String name;
+    private Integer credits;
+    private String department;
+}
